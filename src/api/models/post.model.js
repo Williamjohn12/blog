a5645f03343c62e99d7978e01cfd9ef893b62e72
will_spradlin <williamspradlin039@gmail.com>
2025-06-04 14:44:19 -0400

    fix:post renaming

--- src/api/models/post.model.js
+++ src/api/models/post.model.js
@@ -3,25 +3,25 @@ import { getDbClient } from "../lib/db.js";
 const client = getDbClient();
 
 export const getAllPosts = async () => {
-    const result = await client.query('SELECT * FROM "Post" ');
+    const result = await client.query('SELECT * FROM "post" ');
     
 
     return result.rows;
 }
 
 export const getPostById = async (id) => {
-    const result = await client.query('SELECT * FROM "Post" WHERE id = $1', [id]);
+    const result = await client.query('SELECT * FROM "post" WHERE id = $1', [id]);
     return result.rows[0];
 }
 
 export const deletePostById = async (id) => {
-    const result = await client.query('DELETE FROM "Post" WHERE id = $1', [id]);
+    const result = await client.query('DELETE FROM "post" WHERE id = $1', [id]);
     return result.rowCount > 0;
 }
 
 export const createPost = async (post) => {
     const result = await client.query(
-        'INSERT INTO "Post" (title, content, description) VALUES ($1, $2, $3) RETURNING *',
+        'INSERT INTO "post" (title, content, description) VALUES ($1, $2, $3) RETURNING *',
         [post.title, post.content, post.description]
     );
     return result.rows[0];
